219eeee52cc98166b1572b4dcff8c4a4b091b4e8
AlexJHZheng <autumatic@gmail.com>
2023-05-04 21:45:01 -0300

    完善登录接口

--- api/user.js
+++ api/user.js
@@ -30,15 +30,36 @@ exports.login=async ctx=>{
     //获取请求的用户信息，并解析出用户名和密码
     const userInfo=ctx.request.body
     const username=userInfo.username
+    //用户名为空则返回用户名不能为空
+    if(!username){
+        return ctx.body={status:-2,msg:'用户名不能为空'}
+    }
     const password=userInfo.password
+    //密码为空则返回密码不能为空
+    if(!password){
+        return ctx.body={status:-3,msg:'密码不能为空'}
+    }
     //验证
-    userdb.checkUserLogin(username,password).then((res)=>{
-        console.log('查询用户',username)
-        console.log('查询结果',res)
+    const result = await userdb.checkUserLogin(username,password).then((res)=>{
+        return res
     })
-    
-
-    ctx.body='res'
+    var res=''
+    console.log(result,'result')
+    //result为0返回用户名密码错误
+    if(result==0){
+        res={status:1,msg:'用户名或密码错误'}
+    }
+    //result为-1返回用户被禁用
+    if(result==-1){
+        res={status:-1,msg:'用户被禁用'}
+    }
+    //result为1返回登录成功
+    if(result==1){
+        const token=jwt.sign({username:userInfo.username},config.secretKey,{expiresIn:'24h'})
+        const refreshToken=jwt.sign({username:userInfo.username},config.secretKey,{expiresIn:'30d'})
+        res={status:200,msg:'登录成功',token:token,refreshToken:refreshToken}
+    }
+    ctx.body=res
 }
 // eu vou caucular 10 vezes 400
 exports.cauculator=async ctx=>{
