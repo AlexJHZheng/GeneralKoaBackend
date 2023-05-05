76f86fb61551d96dddb44bb98ae52a86adc2b899
AlexJHZheng <autumatic@gmail.com>
2023-05-05 16:47:30 -0300

    功能更新

--- db/user.js
+++ db/user.js
@@ -2,7 +2,7 @@ const db=require('../db/index')
 
 //查询用户是否存在
 async function checkUserName(user){ //为真表示用户名存在
-    const sql = `select * from user where userName='${user}'`
+    const sql = `select * from User where userName='${user}'`
     const result = await db(sql)
     if(result.length>0){
         return true
@@ -25,9 +25,32 @@ async function checkUserLogin(user,password){
     return 0
 }
 
+//注册用户
+//接收用户名,密码，summary
+//数据库中插入用户名，密码，status为1，createTime为当前时间,summary
+async function register(user,password,summary){
+    const sql = `insert into User(userName,password,status,createTime,summary) values('${user}','${password}',1,now(),'${summary}')`
+    const result = await db(sql)
+    if(result.affectedRows>0){
+        return true
+    }
+    return false
+}
 
+//通过用户名获取用户ID
+async function getUserID(user){
+    const sql = `select * from User where userName='${user}'`
+    const result = await db(sql)
+    if(result.length>0){
+        return result[0].ID
+    }
+    return false
+}
 
 module.exports={
     checkUserName:checkUserName,
-    checkUserLogin:checkUserLogin
+    checkUserLogin:checkUserLogin,
+    register:register,
+    getUserID:getUserID
+
 }
