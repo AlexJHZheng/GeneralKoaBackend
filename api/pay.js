d963940710eff4251457a40f4e8db53b5ed865e3
AlexJHZheng <autumatic@gmail.com>
2023-05-30 16:51:48 -0300

    功能完善

--- api/pay.js
+++ api/pay.js
@@ -37,7 +37,6 @@ exports.addPayFlow = async ctx => {
         console.log(res, '用户ID')
         return res
     })
-
     // 验证
     if (!payTotal) {
         return ctx.body = { status: -1, msg: '支付金额不能为空' }
@@ -45,9 +44,17 @@ exports.addPayFlow = async ctx => {
     if (!userID) {
         return ctx.body = { status: -2, msg: '用户ID不能为空' }
     }
+    // 向银行请求支付，获取二维码链接和支付订单号
+    // pix_path 订单号 
+    const pix_path = 'kk6g232xel65a0daee4dd13kk471542221'
+    // pix_copy 二维码信息文件
+    // pix_wallet 二维码图片地址
+
+
+
+
     
-    
-    const result = await paydb.addPayFlow(payTotal, userID, payObs,payNum,payClient).then((res) => {
+    const result = await paydb.addPayFlow(payTotal, userID, payObs,payNum,payClient,pix_path).then((res) => {
         return res
     })
     if (result) {
@@ -79,5 +86,53 @@ exports.updatePayStatus = async ctx => {
         return ctx.body = { status: 200, msg: '支付流水状态修改成功' }
     } else {
         return ctx.body = { status: -3, msg: '支付流水状态修改失败' }
+    }
+}
+
+// 获取支付流水列表
+exports.getPayFlowList = async ctx => {
+    // 先获取用户ID
+    // 接着获取用户角色
+    // 调用db/pay.js中的getPayFlowList方法
+    // 返回结果
+    const token = ctx.header.authorization
+    // 获取时间段
+    // const time = ctx.request.query
+    console.log(ctx.request.query)
+    // 如果ctx.request.query存在，则解析出startTime和endTime
+    let startTime = ''
+    let endTime = ''
+    if (ctx.request.query) {
+        startTime = ctx.request.query.startTime
+        endTime = ctx.request.query.endTime
+    }
+    // 截取掉Bearer和空格
+    const tokenStr = token.substring(7)
+    // 解析token,获取用户名
+    const username = jwt.decode(tokenStr, config.secretKey).username // 解密，获取payload
+    // 根据用户名获取用户ID
+    const userID = await userdb.getUserID(username).then((res) => {
+        return res
+    }
+    )
+    // 根据用户ID获取用户角色
+    const userRole = await userdb.getUserRoles(username).then((res) => {
+        console.log(res, '用户角色')
+        return res
+    }
+    )
+    // 调用db/pay.js中的getPayFlowList方法
+    const result = await paydb.getPayList(userRole, userID,startTime,endTime).then((res) => {
+        return res
+    }
+    )
+
+
+
+    if (result) {
+        return ctx.body = { status: 200, msg: '支付流水列表获取成功', data: result }
+    }
+    else {
+        return ctx.body = { status: -1, msg: '支付流水列表获取失败' }
     }
 }
--- db/pay.js
+++ db/pay.js
@@ -3,8 +3,9 @@ const db=require('../db/index')
 // 数据库PayFlow结构为payTotal,payDate,userID,payStatus,payObs
 // 新增支付流水，时间为当前时间，状态为0
 // 传入数据为payTotal,userID,payObs
-async function addPayFlow(payTotal,userID,payObs,payNum,payClient){
-    const sql = `insert into PayFlow(payTotal,payDate,userID,payStatus,payObs,payNum,payClient) values('${payTotal}',now(),'${userID}',0,'${payObs}','${payNum}','${payClient}')`
+async function addPayFlow(payTotal,userID,payObs,payNum,payClient,pix_path){
+    const sql = `insert into PayFlow(payTotal,payDate,userID,payStatus,payObs,payNum,payClient,pix_path,payTotalReceved,recStatus,checkStatus) 
+    values('${payTotal}',now(),'${userID}',1,'${payObs}','${payNum}','${payClient}','${pix_path}',0,0,0)`
     const result = await db(sql)
     if(result.affectedRows>0){
         return true
@@ -32,9 +33,83 @@ async function updatePayStatus(payNum,payStatus){
     }
     return false
 }
+// 通过传入的用户权限(admin,editot)和用户ID来获取订单列表，如果是admin则获取所有订单，如果是editor则获取该用户的订单
+// 传入数据为userRole,userID
+async function getPayList(userRole,userID,startTime,endTime){
+    // 如果startTime和endTime存在，则查询时间段内的订单，如果不存在，则查询所有订单
+    // 查询要联合User表获取summary信息,其中User表中ID和payFlow表中的userID相同
+    // 如果是admin，则查询所有订单
+    // 如果是editor，则查询该用户的订单
+    // 查询结果取 PayFlow中的payNum,payTotal,payDate,payStatus,payObs,payClient,pix_path,payTotalReceved和User中的username,summary
+    let sql = ''
+    if(userRole=='admin'){
+        if(startTime&&endTime){
+            sql = `
+      SELECT PayFlow.payNum, PayFlow.payTotal, PayFlow.payDate, PayFlow.payStatus, PayFlow.payObs,
+      PayFlow.payClient, PayFlow.pix_path, PayFlow.payTotalReceved, User.username, User.summary
+      FROM PayFlow
+      LEFT JOIN User ON PayFlow.userID = User.ID
+      WHERE PayFlow.payDate BETWEEN '${startTime}' AND '${endTime}'
+      ORDER BY PayFlow.payDate ASC
+    `
+        }else{
+            sql = `
+      SELECT PayFlow.payNum, PayFlow.payTotal, PayFlow.payDate, PayFlow.payStatus, PayFlow.payObs,
+      PayFlow.payClient, PayFlow.pix_path, PayFlow.payTotalReceved, User.username, User.summary
+      FROM PayFlow
+      LEFT JOIN User ON PayFlow.userID = User.ID
+      ORDER BY PayFlow.payDate ASC
+    `
+        }
+    }else if(userRole=='editor'){
+        if(startTime&&endTime){
+            sql = `
+      SELECT PayFlow.payNum, PayFlow.payTotal, PayFlow.payDate, PayFlow.payStatus, PayFlow.payObs,
+      PayFlow.payClient, PayFlow.pix_path, PayFlow.payTotalReceved, User.username, User.summary
+      FROM PayFlow
+      LEFT JOIN User ON PayFlow.userID = User.ID
+      WHERE PayFlow.payDate BETWEEN '${startTime}' AND '${endTime}' AND PayFlow.userID = '${userID}'
+      ORDER BY PayFlow.payDate ASC
+    `
+        }else{
+            sql = `
+      SELECT PayFlow.payNum, PayFlow.payTotal, PayFlow.payDate, PayFlow.payStatus, PayFlow.payObs,
+      PayFlow.payClient, PayFlow.pix_path, PayFlow.payTotalReceved, User.username, User.summary
+      FROM PayFlow
+      LEFT JOIN User ON PayFlow.userID = User.ID
+      WHERE PayFlow.userID = '${userID}'
+      ORDER BY PayFlow.payDate ASC
+    `
+        }
+    }
+    const result = await db(sql)
+    if(result.length>0){
+        return result
+    }
+    return false
+    // 之前的代码
+
+
+
+
+    // let sql = ''
+    // if(userRole=='admin'){
+    //     sql = `select * from PayFlow`
+    // }else if(userRole=='editor'){
+    //     sql = `select * from PayFlow where userID='${userID}'`
+    // }
+    // const result = await db(sql)
+    // if(result.length>0){
+    //     return result
+    // }
+    // return false
+}
+
+
 
 module.exports={
     addPayFlow:addPayFlow,
     checkPayNum:checkPayNum,
-    updatePayStatus:updatePayStatus
+    updatePayStatus:updatePayStatus,
+    getPayList:getPayList
 }
