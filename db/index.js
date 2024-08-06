639055f3baca8255d4427ab48b13e0f8566fbf47
AlexJHZheng <autumatic@gmail.com>
2024-08-06 11:20:34 -0300

    使用环境变量加密

--- db/index.js
+++ db/index.js
@@ -1,34 +1,34 @@
 //数据库连接模块
 
 //导入mysql模块
-const mysql=require('mysql')
+const mysql = require("mysql");
 const pool = mysql.createPool({
-     //数据库IP
-     host:'177.71.131.255',
-     //数据库账号
-     user:'admin',
-     //数据库密码
-     password:'nmYrzEL2GzZyGFw8',
-     //数据库名称
-     database:'pix'
-  })
-  //返回Promise，使用连接池，连接查询mysql
-  const query = function(sql) {
-    return new Promise((resolve, reject) => {
-      pool.getConnection(function(err, connection) {
-        if (err) {
-          reject(err)
-        } else {
-          connection.query(sql, (err, results) => {
-            if (err) {
-              reject(err)
-            } else {
-              resolve(results)
-            }
-            connection.release()
-          })
-        }
-      })
-    })
-  }
-module.exports=query
+  //数据库IP
+  host: process.env.DB_HOST,
+  //数据库账号
+  user: process.env.DB_USER,
+  //数据库密码
+  password: process.env.DB_PASSWORD,
+  //数据库名称
+  database: process.env.DB_DATABASE,
+});
+//返回Promise，使用连接池，连接查询mysql
+const query = function (sql) {
+  return new Promise((resolve, reject) => {
+    pool.getConnection(function (err, connection) {
+      if (err) {
+        reject(err);
+      } else {
+        connection.query(sql, (err, results) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(results);
+          }
+          connection.release();
+        });
+      }
+    });
+  });
+};
+module.exports = query;
