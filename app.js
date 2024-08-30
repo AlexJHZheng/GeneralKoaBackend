d98a086fdacb89297c7feea25a2ca7559e979cbc
AlexJHZheng <autumatic@gmail.com>
2024-08-30 15:01:05 -0300

    update respost

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
-const Koa = require('koa') 
-const cors = require('koa2-cors')
-const bodyParser = require('koa-bodyparser')
-const user_router=require('./router/index')
-const koaJWT = require('koa-jwt') //token解密模块
-const config = require('./config')
+const Koa = require("koa");
+const cors = require("koa2-cors");
+const bodyParser = require("koa-bodyparser");
+const user_router = require("./router/index");
+const koaJWT = require("koa-jwt"); //token解密模块
+const config = require("./config");
+const timenow = new Date();
 
-const app =new Koa() //项目入口
+const app = new Koa(); //项目入口
 
 // 除了这个地址，其他的URL都需要验证
 // app.use(koaJWT({ secret: config.secretKey }).unless({ path: [/^\/api\/(login|refresh)/] }))
 
 // 错误处理
-const errFn=async (ctx,next)=>{
-    try{
-        await next()
-    }catch(err){
-        //错误
-        ctx.body={status:1,msg:err.message}
-    }
-}
+const errFn = async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    //错误
+    ctx.body = { status: 1, msg: err.message };
+  }
+};
 
+app.use(errFn); //错误处理
+app.use(cors()); //跨域
+app.use(bodyParser()); //解析接口传递body
+app.use(user_router.routes()); //注册路由
 
-app.use(errFn) //错误处理
-app.use(cors()) //跨域
-app.use(bodyParser()) //解析接口传递body
-app.use(user_router.routes()) //注册路由
-
-app.listen(8609,()=>{ //端口
-    console.log('服务器启动成功')
-})
+app.listen(8609, () => {
+  //端口
+  console.log(timenow + "PixApi服务器启动成功,服务端口8609!");
+});
